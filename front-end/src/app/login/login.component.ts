57a6277ce9821ca8e6f7d0ec87bdf7909927311e
Thi Thanh Tu DUONG <thi-thanh-tu.duong@etu.univ-cotedazur.fr>
2023-03-15 18:15:32 +0100

    feat: Login frontend up #6

---
+++ front-end/src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+
+
+}
