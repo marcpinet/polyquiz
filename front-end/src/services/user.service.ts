67ea35223102c915422cfd689779279217c22604
Thi Thanh Tu DUONG <thi-thanh-tu.duong@etu.univ-cotedazur.fr>
2023-03-18 08:03:13 +0100

    feat: createResident plus complet #6

--- front-end/src/services/user.service.ts
+++ front-end/src/services/user.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject, Subject,  Observable, forkJoin } from 'rxjs';
 import { User } from '../models/user.model';
 import { Resident } from '../models/resident.model';
 import { serverUrl, httpOptionsBase } from '../configs/server.config';
 import { Router } from '@angular/router';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -24,29 +23,61 @@ export class UserService {
   }
 
   retrieveUsers(): void {
-    this.http.get<User[]>(this.userUrl).subscribe((userList) => {
-      this.users = userList;
-      this.users$.next(this.users);
+    this.http.get<User[]>(this.userUrl).subscribe({
+      next: userList => {
+        this.users = userList;
+        this.users$.next(this.users);
+      },
+      error: error => {
+        console.error('Failed to retrieve users', error);
+      },
+      complete: () => {
+        console.log('User retrieval completed');
+      }
     });
   }
 
   retrieveResidents(): void {
-    this.http.get<Resident[]>(this.residentUrl).subscribe((residentList) => {
-      this.residents = residentList;
-      this.residents$.next(this.residents);
+    this.http.get<Resident[]>(this.residentUrl).subscribe({
+      next: residentList => {
+        this.residents = residentList;
+        this.residents$.next(this.residents);
+      },
+      error: error => {
+        console.error('Failed to retrieve residents', error);
+      },
+      complete: () => {
+        console.log('Resident retrieval completed');
+      }
     });
   }
 
-  addUser(user: User): void {
-    this.http.post<User>(this.userUrl, user, this.httpOptions).subscribe(() => this.retrieveUsers());
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(this.userUrl, user, this.httpOptions);
   }
 
-  addResident(resident: Resident): void {
-    this.http.post<Resident>(this.residentUrl, resident, this.httpOptions).subscribe(() => this.retrieveResidents());
+  addResident(resident: Resident): Observable<Resident> {
+    return this.http.post<Resident>(this.residentUrl, resident, this.httpOptions);
   }
 
   createResident(resident: Resident, user: User): void {
-    this.addUser(user);
-    this.addResident(resident);
+    forkJoin([ //create both resident and user or none
+      this.addUser(user),
+      this.addResident(resident)
+    ]).subscribe({
+      next: ([addedUser, addedResident]) => {
+        console.log('Both user and resident were added successfully');
+        this.users.push(addedUser);
+        this.users$.next(this.users);
+        this.residents.push(addedResident);
+        this.residents$.next(this.residents);
+      },
+      error: error => {
+        console.error('Failed to create resident', error);
+      },
+      complete: () => {
+        console.log('Resident creation completed');
+      }
+    });
   }
 }
