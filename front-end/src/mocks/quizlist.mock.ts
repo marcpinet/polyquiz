54ddb81cc587ca9b3a1c5c2e04dd76e9709f4759
loic pantano <loic.pantano@etu.univ-cotedazur.fr>
2023-03-22 14:01:36 +0100

    feat: add some css for the quiz #19

--- front-end/src/mocks/quizlist.mock.ts
+++ front-end/src/mocks/quizlist.mock.ts
@@ -6,7 +6,8 @@ export const QUIZ_LIST: Quiz[] = [
     img: '../assets/img/placeholder.png',
     name: 'Les Acteurs',
     theme: 'Actor',
-    questions: [{question: "Question 1", answers: ["Réponse 1", "Réponse 2", "Réponse 3", "Réponse 4"], goodAnswer: 0},
+    questions: [{question: "La ville du Vatican, capitale du Vatican, a le taux de crime le plus faible du monde.", 
+    answers: ["Vrai", "Faux"], goodAnswer: 0},
                 {question: "Question 2", answers: ["Réponse 5", "Réponse 6", "Réponse 7", "Réponse 8"], goodAnswer: 1},
                 {question: "Question 3", answers: ["Réponse 9", "Réponse 10", "Réponse 11", "Réponse 12"], goodAnswer: 2},
                 {question: "Question 4", answers: ["Réponse 13", "Réponse 14", "Réponse 15", "Réponse 16"], goodAnswer: 3}]
