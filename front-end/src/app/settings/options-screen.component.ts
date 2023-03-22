5d86fc7703e09451ddc550768984cf61aa06ab30
Marc <marc.pinet@etu.univ-cotedazur.fr>
2023-03-22 11:42:27 +0100

    feat: add background color & button functions #16

--- front-end/src/app/settings/options-screen.component.ts
+++ front-end/src/app/settings/options-screen.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, RendererFactory2 } from '@angular/core';
 
 @Component({
   selector: 'app-options-screen',
   templateUrl: './options-screen.component.html',
 })
 export class OptionsScreenComponent {
+  private renderer: Renderer2;
+
   public defaultSettings = {
     soundEffects: true,
     spacebarClick: false,
@@ -15,15 +17,22 @@ export class OptionsScreenComponent {
 
   public settings = { ...this.defaultSettings };
 
+  constructor(rendererFactory: RendererFactory2, private el: ElementRef) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+  }
+
   public resetSettings(): void {
     this.settings = { ...this.defaultSettings };
   }
 
   public saveSettings(): void {
     console.log('Settings saved:', this.settings);
+
+    // TODO: Update the settings
   }
 
   public settingsChanged(): boolean {
     return JSON.stringify(this.settings) !== JSON.stringify(this.defaultSettings);
   }
+
 }
