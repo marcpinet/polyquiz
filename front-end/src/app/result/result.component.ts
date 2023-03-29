81ba5cc8c7d1f3979584f425c7cccc7547e61651
loic pantano <loic.pantano@etu.univ-cotedazur.fr>
2023-03-29 20:08:18 +0200

    feat: result dyn #42

--- front-end/src/app/result/result.component.ts
+++ front-end/src/app/result/result.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Result } from 'src/models/result-quiz.model';
+import { ResultService } from 'src/services/result.service';
 
 @Component({
     selector: 'app-result',
@@ -11,14 +14,32 @@ export class ResultComponent implements OnInit {
     score: number = this.correct_answers / (this.correct_answers + this.incorrect_answers) * 100;
     total_time = 10;
     average_time = 1;
+    result: Result;
 
-    constructor() {
+    constructor(private route: ActivatedRoute, private router: Router, private resultService: ResultService) {
+        const id = this.route.snapshot.paramMap.get('id');
+        this.resultService.setSelectedResult(id);
+        this.resultService.resultSelected$.subscribe((result) => this.result = result);
+
+        this.resultService.resultSelected$.subscribe((result) => this.result = result);
     }
 
     ngOnInit() {
 
     }
 
+    compilTime(time: number){
+        var tmp = "";
+        if(time > 60){
+            tmp += Math.floor(time/60) + "m";
+        }
+        tmp+= (time%60).toFixed(2) +"s"
+        return tmp;
+    }
+
+    replay(){
+        this.router.navigate(['/']);
+    }
 
 
 }
