f4d67785daa7815bb48a4acb99a2e4bdc0282911
Thi Thanh Tu DUONG <thi-thanh-tu.duong@etu.univ-cotedazur.fr>
2023-04-06 02:22:36 +0200

    feat: affichage de bonne reponse #42

--- frontend/src/app/gameComponents/game-question/game-question.component.ts
+++ frontend/src/app/gameComponents/game-question/game-question.component.ts
@@ -1,14 +1,13 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Question } from 'src/models/quiz.model';
 import { GamePageComponent } from '../game-page/game-page.component';
-
 @Component({
   selector: 'app-game-question',
   templateUrl: './game-question.component.html',
 })
-export class GameQuestionComponent {
+export class GameQuestionComponent implements OnInit {
   answerSelected = -1;
-
+  goodAnswer: string;
   @Input()
   question: Question;
   @Output()
@@ -18,13 +17,26 @@ export class GameQuestionComponent {
     this.question = {} as Question;
   }
 
+  ngOnInit(): void {
+    this.updateGoodAnswer();
+  }
+
+  updateGoodAnswer() {
+    this.goodAnswer = this.question.answers.find(
+      (answer) => answer.isCorrect
+    ).answer_text;
+  }
+
+
+
   checkAnswer(answer: number) {
     this.answerSelected = answer;
   }
 
   nextQuestion() {
-    let ans = this.answerSelected;
+    this.selectAnswer.emit(this.answerSelected);
     this.answerSelected = -1;
-    this.selectAnswer.emit(ans);
+    this.updateGoodAnswer();
   }
+
 }
