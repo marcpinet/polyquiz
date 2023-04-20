43e9c6094caecad1db08e9e4f167f498fd270a00
Marc Pinet <marc.pinet@etu.univ-cotedazur.fr>
2023-04-20 02:42:27 +0200

    feat: better button identification #98

--- frontend/src/app/gameComponents/game-question/game-question.component.ts
+++ frontend/src/app/gameComponents/game-question/game-question.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Question } from 'src/models/quiz.model';
 import { GamePageComponent } from '../game-page/game-page.component';
 import { SettingService } from 'src/services/settings.service';
+import { writtenNumber } from 'written-number';
 
 @Component({
   selector: 'app-game-question',
@@ -35,5 +36,25 @@ export class GameQuestionComponent implements OnInit {
   nextQuestion() {
     this.selectAnswer.emit(this.answerSelected);
     this.answerSelected = -1;
+  }
+
+  protected normalizeText(text: string): string {
+    const accents = 'ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ';
+    const noAccents = 'AAAAAAaaaaaaOOOOOOooooooEEEEeeeeCcIIIIiiiiUUUUuuuuyNn';
+    const newText = text
+      .trim()
+      .replace(/\d+/g, (match) =>
+        writtenNumber(parseInt(match), { lang: 'fr' })
+      )
+      .split('')
+      .map((char) => {
+        const index = accents.indexOf(char);
+        return index !== -1 ? noAccents[index] : char;
+      })
+      .join('')
+      .toLowerCase()
+      .replace(/\s+/g, '_')
+      .replace(/[^a-zA-Z0-9_]/g, '_');
+    return newText;
   }
 }
