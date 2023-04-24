5c56bd56b2c2d8551baeb5b86ed76c184a5c4d7b
Thi Thanh Tu DUONG <thi-thanh-tu.duong@etu.univ-cotedazur.fr>
2023-04-24 11:50:53 +0200

    feat: confirm answers done #81

--- frontend/src/app/gameComponents/game-question/game-question.component.ts
+++ frontend/src/app/gameComponents/game-question/game-question.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Question } from 'src/models/quiz.model';
 import { GamePageComponent } from '../game-page/game-page.component';
-import { SettingService } from 'src/services/settings.service';
 import { writtenNumber } from 'written-number';
-
+import { Subscription } from 'rxjs';
+import { SettingService } from 'src/services/settings.service';
+import Swal from 'sweetalert2';
+import { Settings } from 'src/models/settings.model';
 @Component({
   selector: 'app-game-question',
   templateUrl: './game-question.component.html',
@@ -11,13 +13,23 @@ import { writtenNumber } from 'written-number';
 export class GameQuestionComponent implements OnInit {
   answerSelected = -1;
   microphoneActivated = false;
+  private userSettings: Settings;
+  private settingsSubscription: Subscription;
+
   @Input()
   question: Question;
   @Output()
   selectAnswer = new EventEmitter<number>();
 
-  constructor(private settingService: SettingService) {
+  constructor(private settingsService: SettingService) {
     this.question = {} as Question;
+    this.settingsService.setCurrentUserSettings().then(() => {
+      this.settingsSubscription = this.settingsService.settings$.subscribe(
+        (settings) => {
+          this.userSettings = settings;
+        }
+      );
+    });
   }
 
   ngOnInit(): void {}
@@ -36,6 +48,39 @@ export class GameQuestionComponent implements OnInit {
   nextQuestion() {
     this.selectAnswer.emit(this.answerSelected);
     this.answerSelected = -1;
+  }
+
+  selectAnswerClick(answer: number) {
+    if (this.userSettings.confirm_answer) {
+      let htmlTxt = `<h1 class="text-6xl text-[#2B3467] font-bold mb-4">Vous voulez choisir ${this.question.answers[answer].answer_text} ?</h1>`;
+      if (this.question.answers[answer].answer_image) {
+        htmlTxt += `<img src="${this.question.answers[answer].answer_image}" class="w-1/2 mx-auto">`;
+      }
+      Swal.fire({
+        html: htmlTxt,
+        showDenyButton: true,
+        showCancelButton: false,
+        width: 1700,
+        padding: '4em',
+        confirmButtonText:
+          '<span style="font-size: 150px; padding: 150px 150px; ">Oui</span>',
+        denyButtonText:
+          '<span style="font-size: 150px; padding: 150px 150px;">Non</span>', // add non-breaking spaces between span tags to create more space between buttons
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire({
+            icon: 'success',
+            title: 'Votre réponse a été enregistrée',
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          this.answerSelected = answer;
+        } else if (result.isDenied) {
+          Swal.fire("La réponse n'a pas été enregistrée", '', 'info');
+        }
+      });
+    }
   }
 
   protected normalizeText(text: string): string {
