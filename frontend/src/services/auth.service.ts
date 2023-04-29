5f866ade3d08dec1231a4ef99f8c6f8a9db02ca1
Thi Thanh Tu DUONG <thi-thanh-tu.duong@etu.univ-cotedazur.fr>
2023-04-29 10:12:25 +0200

    feat: admin connection finished #147

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,16 +1,16 @@
-import {Injectable} from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { User } from 'src/models/user.model';
-import {httpOptionsBase, serverUrl} from '../configs/server.config';
-import {HttpClient} from '@angular/common/http';
-import {Router} from '@angular/router';
+import { httpOptionsBase, serverUrl } from '../configs/server.config';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 //TODO: Login redirects based on resident or admin
 export class AuthService {
-  private authUrl = serverUrl + '/users'
+  private authUrl = serverUrl + '/users';
   private loginPath = 'login';
   private logoutPath = 'logout';
   private httpOptions = httpOptionsBase;
@@ -18,34 +18,38 @@ export class AuthService {
   public user: User = null;
   public user$: BehaviorSubject<User> = new BehaviorSubject<User>(this.user);
 
-  constructor(private http: HttpClient,
-    private router: Router) {
-  }
+  constructor(private http: HttpClient, private router: Router) {}
 
   isLoggedIn(): boolean {
     return this.user !== null && this.user.hasOwnProperty('id');
   }
 
   login(user: User) {
-    return new Promise(
-      ((resolve, reject) => {
-        this.http.post<User>(this.authUrl + '/' + this.loginPath, user, httpOptionsBase).subscribe((user) => {
+    return new Promise((resolve, reject) => {
+      this.http
+        .post<User>(this.authUrl + '/' + this.loginPath, user, httpOptionsBase)
+        .subscribe((user) => {
           this.user = user;
           this.user$.next(this.user);
           if (this.isLoggedIn()) {
-            this.router.navigate(['/']);
+            if (user.userType === 'admin') {
+              this.router.navigate(['/admin']);
+            } else {
+              this.router.navigate(['/']);
+            }
             resolve('');
           } else {
             resolve('Erreur : ' + JSON.parse(JSON.stringify(user)).errors);
           }
         });
-      }));
+    });
   }
 
   getLogin() {
-    return new Promise(
-      ((resolve, reject) => {
-        this.http.get(this.authUrl + '/' + this.loginPath, httpOptionsBase).subscribe((user) => {
+    return new Promise((resolve, reject) => {
+      this.http
+        .get(this.authUrl + '/' + this.loginPath, httpOptionsBase)
+        .subscribe((user) => {
           this.user = user as User;
           this.user$.next(this.user);
           if (this.isLoggedIn()) {
@@ -54,15 +58,16 @@ export class AuthService {
             resolve('');
           }
         });
-      }));
-  }
-
-  logout() {
-    this.http.get(this.authUrl + '/' + this.logoutPath, httpOptionsBase).subscribe(() => {
-      this.user = null;
-      this.user$.next(this.user);
-      this.router.navigate(['/login']);
     });
   }
 
+  logout() {
+    this.http
+      .get(this.authUrl + '/' + this.logoutPath, httpOptionsBase)
+      .subscribe(() => {
+        this.user = null;
+        this.user$.next(this.user);
+        this.router.navigate(['/login']);
+      });
+  }
 }
