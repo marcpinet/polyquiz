f9b6a21eb6d1cb588c3fcbba355fd989ab9cde01
Thi Thanh Tu DUONG <thi-thanh-tu.duong@etu.univ-cotedazur.fr>
2023-05-06 08:44:41 +0200

    feat: finished gestion quiz base pages #176

---
+++ frontend/src/app/admin/themes/themes.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-themes',
+  templateUrl: './themes.component.html',
+})
+export class ThemesComponent {
+  constructor(private router: Router) {}
+}
