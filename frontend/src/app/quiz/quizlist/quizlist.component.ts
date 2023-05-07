9fc5fd07c9900726fcbbf00b6b352074846349e5
Marc Pinet <marc.pinet@etu.univ-cotedazur.fr>
2023-05-08 01:35:49 +0200

    feat: add gray filter on already played quiz #182

--- frontend/src/app/quiz/quizlist/quizlist.component.ts
+++ frontend/src/app/quiz/quizlist/quizlist.component.ts
@@ -4,6 +4,10 @@ import { QuizService } from '../../../services/quiz.service';
 import { Quiz } from 'src/models/quiz.model';
 import { ThemesService } from 'src/services/theme.service';
 import { Theme } from 'src/models/quiz.model';
+import { AuthService } from 'src/services/auth.service';
+import { ResultService } from 'src/services/result.service';
+import { User } from 'src/models/user.model';
+
 @Component({
   selector: 'quizlist',
   templateUrl: './quizlist.component.html',
@@ -11,17 +15,33 @@ import { Theme } from 'src/models/quiz.model';
 })
 export class QuizListComponent implements OnInit {
   public quizList: Quiz[] = [];
+  public playedQuizIds: string[] = [];
+  public user: User;
   public themes: Theme[] = [];
 
   constructor(
     private router: Router,
     public quizService: QuizService,
-    private themeService: ThemesService
+    private themeService: ThemesService,
+    private authService: AuthService,
+    private resultService: ResultService
   ) {
     this.quizService.quizzes$.subscribe((quizzes: Quiz[]) => {
       this.quizList = quizzes;
       this.populateThemes();
     });
+
+    this.authService.user$.subscribe((user) => {
+      this.user = user;
+
+      this.resultService.getResultsByUser(user).subscribe((results) => {
+        this.playedQuizIds = results.map((result) => result.quiz_id);
+      });
+    });
+  }
+
+  hasPlayedQuiz(quizId: string): boolean {
+    return this.playedQuizIds.includes(quizId);
   }
 
   populateThemes(): void {
