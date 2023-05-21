333e148145b81fb766863a544f96c282ca25025e
Marc Pinet <marc.pinet@etu.univ-cotedazur.fr>
2023-05-22 00:16:37 +0200

    fix: useless resident infos won't be rendered for admins profiles #260

--- frontend/src/app/userProfile/user-profile.component.ts
+++ frontend/src/app/userProfile/user-profile.component.ts
@@ -5,6 +5,7 @@ import { Resident } from 'src/models/resident.model';
 import { UserService } from 'src/services/user.service';
 import { QuizService } from 'src/services/quiz.service';
 import { ResultService } from 'src/services/result.service';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'user-profile',
@@ -26,65 +27,72 @@ export class UserProfileComponent implements OnInit {
       this.user = user;
     });
     this.userService.residents$.subscribe((residents) => {
-      this.resident = residents.find(
-        (resident) => resident.userId === this.user.id
-      );
-    });
-
-    this.resultService
-      .getResultsByUser(this.user)
-      .subscribe(async (results) => {
-        let totalQuestions = 0;
-        let totalCorrectAnswers = 0;
-        let totalPoints = 0;
-
-        let quizIds = new Set<string>();
-        for (const element of results) {
-          let result = element;
-          quizIds.add(result.quiz_id);
-        }
-
-        let quizIdsArray = Array.from(quizIds);
-        for (const element of quizIdsArray) {
-          let quizId = element;
-          const quiz = await this.quizService.getQuizById(quizId).toPromise();
-          let quizResults = results.filter(
-            (result) => result.quiz_id === quizId
-          );
-
-          for (const result of quizResults) {
-            const questions = result.right_answers + result.wrong_answers;
-            totalQuestions += questions;
-            totalCorrectAnswers += result.right_answers;
-
-            let pointMultiplier = 0;
-            switch (quiz.difficulty) {
-              case 'Facile':
-                pointMultiplier = 100;
-                break;
-              case 'Moyen':
-                pointMultiplier = 200;
-                break;
-              case 'Difficile':
-                pointMultiplier = 300;
-                break;
-              default:
-                break;
-            }
-
-            const correctPercentage = result.right_answers / questions;
-            totalPoints += correctPercentage * pointMultiplier;
-          }
-        }
-
-        this.totalScore = Math.round(totalPoints);
-        this.successRate = (totalCorrectAnswers / totalQuestions) * 100;
+      combineLatest([
+        this.authService.user$,
+        this.userService.residents$,
+      ]).subscribe(([user, residents]) => {
+        this.user = user;
+        this.resident = residents.find(
+          (resident) => resident.userId === user.id
+        );
       });
-  }
 
-  ngOnInit() {}
+      this.resultService
+        .getResultsByUser(this.user)
+        .subscribe(async (results) => {
+          let totalQuestions = 0;
+          let totalCorrectAnswers = 0;
+          let totalPoints = 0;
+
+          let quizIds = new Set<string>();
+          for (const element of results) {
+            let result = element;
+            quizIds.add(result.quiz_id);
+          }
+
+          let quizIdsArray = Array.from(quizIds);
+          for (const element of quizIdsArray) {
+            let quizId = element;
+            const quiz = await this.quizService.getQuizById(quizId).toPromise();
+            let quizResults = results.filter(
+              (result) => result.quiz_id === quizId
+            );
+
+            for (const result of quizResults) {
+              const questions = result.right_answers + result.wrong_answers;
+              totalQuestions += questions;
+              totalCorrectAnswers += result.right_answers;
+
+              let pointMultiplier = 0;
+              switch (quiz.difficulty) {
+                case 'Facile':
+                  pointMultiplier = 100;
+                  break;
+                case 'Moyen':
+                  pointMultiplier = 200;
+                  break;
+                case 'Difficile':
+                  pointMultiplier = 300;
+                  break;
+                default:
+                  break;
+              }
+
+              const correctPercentage = result.right_answers / questions;
+              totalPoints += correctPercentage * pointMultiplier;
+            }
+          }
+
+          this.totalScore = Math.round(totalPoints);
+          this.successRate = (totalCorrectAnswers / totalQuestions) * 100;
+        });
+    });
+  }
 
   logout() {
     this.authService.logout();
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 }
