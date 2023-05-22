cb3b866713e014ff06c85c4ab9f1740c9ecfce44
Thi Thanh Tu DUONG <thi-thanh-tu.duong@etu.univ-cotedazur.fr>
2023-05-22 08:10:31 +0200

    feat: update settings service #240

--- backend/app/build-server.js
+++ backend/app/build-server.js
@@ -12,7 +12,25 @@ const corsOptions = {
 module.exports = (cb) => {
   const app = express();
   app.disable("x-powered-by");
-  app.use(cors(corsOptions));
+  app.use(cors({ credentials: true, origin: "http://localhost:4200" }));
+
+  app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    next();
+  });
   app.use(bodyParser.json({}));
   app.use(
     morgan(
