f253abb235c4058b0173f134459bf97dcb434a73
Marc Pinet <marc.pinet@etu.univ-cotedazur.fr>
2023-05-27 12:49:46 +0200

    feat: add numbered bar to resident list for admin #169

--- frontend/src/app/admin/mesResidents/residentList/resident-list.component.ts
+++ frontend/src/app/admin/mesResidents/residentList/resident-list.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Resident } from 'src/models/resident.model';
 import { UserService } from 'src/services/user.service';
 import { User } from 'src/models/user.model';
+
 @Component({
   selector: 'app-residents-list',
   templateUrl: './resident-list.component.html',
@@ -13,6 +14,9 @@ export class ResidentListComponent {
   public displayedUsers: User[] = [];
   public displayedResident: Resident[] = [];
   public inputValue: string = '';
+
+  private _currentPage: number = 1;
+  public itemsPerPage: number = 4;
 
   constructor(public router: Router, public userService: UserService) {
     this.userService.residents$.subscribe((residents) => {
@@ -25,7 +29,21 @@ export class ResidentListComponent {
         this.displayedUsers = this.usersList;
       }
       this.displayedResident = residents;
+      this.searchResident();
     });
+  }
+
+  public get currentPage(): number {
+    return this._currentPage;
+  }
+
+  public set currentPage(value: number) {
+    this._currentPage = value;
+    this.searchResident();
+  }
+
+  public get totalPages(): number {
+    return Math.ceil(this.residentsList.length / this.itemsPerPage);
   }
 
   navigateResidentStats(userId: number) {
@@ -36,34 +54,48 @@ export class ResidentListComponent {
     if (this.inputValue == '') {
       this.displayedResident = this.residentsList;
       this.displayedUsers = this.usersList;
-      return;
+    } else {
+      const filteredUsers = this.usersList.filter((user) => {
+        if (user == undefined) return false;
+        return (
+          user.firstName
+            .toLowerCase()
+            .includes(this.inputValue.toLowerCase()) ||
+          user.lastName.toLowerCase().includes(this.inputValue.toLowerCase()) ||
+          user.userName.toLowerCase().includes(this.inputValue.toLowerCase())
+        );
+      });
+      const filteredResidents = this.residentsList.filter((resident) => {
+        if (resident == undefined) return false;
+        return resident.id.toString().includes(this.inputValue);
+      });
+      //TODO: pretty sure there is another way shorter lol
+      this.displayedResident = [];
+      this.displayedUsers = [];
+      for (let resident of filteredResidents) {
+        this.displayedUsers.push(
+          this.usersList.filter((user) => user.id === resident.userId)[0]
+        );
+        this.displayedResident.push(resident);
+      }
+      for (let user of filteredUsers) {
+        this.displayedResident.push(
+          this.residentsList.filter(
+            (resident) => resident.userId === user.id
+          )[0]
+        );
+        this.displayedUsers.push(user);
+      }
     }
-    const filteredUsers = this.usersList.filter((user) => {
-      if (user == undefined) return false;
-      return (
-        user.firstName.toLowerCase().includes(this.inputValue.toLowerCase()) ||
-        user.lastName.toLowerCase().includes(this.inputValue.toLowerCase()) ||
-        user.userName.toLowerCase().includes(this.inputValue.toLowerCase())
-      );
-    });
-    const filteredResidents = this.residentsList.filter((resident) => {
-      if (resident == undefined) return false;
-      return resident.id.toString().includes(this.inputValue);
-    });
-    //TODO: pretty sure there is another way shorter lol
-    this.displayedResident = [];
-    this.displayedUsers = [];
-    for (let resident of filteredResidents) {
-      this.displayedUsers.push(
-        this.usersList.filter((user) => user.id === resident.userId)[0]
-      );
-      this.displayedResident.push(resident);
-    }
-    for (let user of filteredUsers) {
-      this.displayedResident.push(
-        this.residentsList.filter((resident) => resident.userId === user.id)[0]
-      );
-      this.displayedUsers.push(user);
-    }
+
+    this.displayedResident = this.displayedResident.slice(
+      (this.currentPage - 1) * this.itemsPerPage,
+      this.currentPage * this.itemsPerPage
+    );
+
+    this.displayedUsers = this.displayedUsers.slice(
+      (this.currentPage - 1) * this.itemsPerPage,
+      this.currentPage * this.itemsPerPage
+    );
   }
 }
