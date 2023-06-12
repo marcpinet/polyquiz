298db0d98ea6a1b87fa9887860dde0f6486e8039
ClementREMY2 <clement.remy@etu.univ-cotedazur.fr>
2023-06-12 16:44:26 +0200

    test: gestion des filtres #295

--- frontend/e2e/scenarios/mainpage-scenario.spec.ts
+++ frontend/e2e/scenarios/mainpage-scenario.spec.ts
@@ -11,12 +11,54 @@ test.describe('Main page tests', () => {
       appComponentFixture.ConnexionAsUser(page);
     });
 
-    await test.step('Check for successful login', async () => {
-      await page.waitForSelector('li#quiz-btn');
+    await test.step('Difficulte', async () => {
+      await page.getByRole('button', { name: 'Difficulté' }).click();
+      //Vérifions que le texte de la popup est bien présente
+      await page.waitForSelector('text=Sélectionnez la difficulté');
+      await page.getByRole('combobox').click();
+      // Sélectionner l'option "Moyen"
+      await page.selectOption('role=combobox', { label: 'Moyen' });
+      await page.getByRole('button', { name: 'OK' }).click();
+      //On verifie que le bouton s'est bien mis a jour
+      await page.waitForSelector('text=Moyen');
     });
 
-    await test.step('Filtres', async () => {
-      await page.click('Difficulté');
+    await test.step('Progres', async () => {
+      await page.getByRole('button', { name: 'Progrès' }).click();
+      await page.waitForSelector('text=Sélectionnez le statut');
+      await page.getByRole('combobox').click();
+      await page.selectOption('role=combobox', { label: 'Non fait' });
+      await page.getByRole('button', { name: 'OK' }).click();
+      //On verifie que le bouton s'est bien mis a jour
+      await page.waitForSelector('text=Non fait');
+    });
+
+    await test.step('Theme', async () => {
+      await page.getByRole('button', { name: 'Thème' }).click();
+      await page.waitForSelector('text=Sélectionnez le thème');
+      await page.getByRole('button', { name: 'OK' }).click();
+      //On verifie que le bouton s'est bien mis a jour
+      await page.waitForSelector('text=Géographie');
+    });
+
+    await test.step('Duree', async () => {
+      await page.getByRole('button', { name: 'Durée' }).click();
+      await page.waitForSelector('text=Sélectionnez la durée');
+      await page.getByRole('combobox').click();
+      await page.selectOption('role=combobox', { label: '> 10 min' });
+      await page.getByRole('button', { name: 'OK' }).click();
+      //On verifie que le bouton s'est bien mis a jour
+      await page.waitForSelector('text=> 10 min');
+    });
+
+    await test.step('Reinitialisation', async () => {
+      await page.getByRole('button', { name: 'Réinitialiser' }).click();
+      //On verifie que le bouton s'est bien mis a jour
+      await page.waitForSelector('text=Difficulté');
+      //Je ne sais pas pourquoi celui ci ne passe pas...
+      //await page.waitForSelector('text=Progrès');
+      await page.waitForSelector('text=Thème');
+      await page.waitForSelector('text=Durée');
     });
   });
 });
