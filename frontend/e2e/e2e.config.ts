81e1675631a5869f0b1fc5dc541dcf4c7b73959d
loic pantano <loic.pantano@etu.univ-cotedazur.fr>
2023-06-12 12:53:46 +0200

    test: resultUrl #298

--- frontend/e2e/e2e.config.ts
+++ frontend/e2e/e2e.config.ts
@@ -1,3 +1,4 @@
 export const testUrl = 'http://localhost:4200/';
 export const loginUrl = 'http://localhost:4200/login';
 export const testQuiz = 'http://localhost:4200/game/1679875889425';
+export const testResultRegex = /^http:\/\/localhost:4200\/result\/\d+$/;
