d1cb5bb1096a4502a2911e687dd36235ecc15983
Thi Thanh Tu DUONG <thi-thanh-tu.duong@etu.univ-cotedazur.fr>
2023-06-16 04:23:55 +0200

    feat: base to modif theme dialog #175

--- frontend/src/app/admin/themes/modifTheme/modif-theme.component.ts
+++ frontend/src/app/admin/themes/modifTheme/modif-theme.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Theme } from 'src/models/quiz.model';
 import { ThemesService } from 'src/services/theme.service';
@@ -7,4 +7,6 @@ import { ThemesService } from 'src/services/theme.service';
   selector: 'app-modif-theme',
   templateUrl: './modif-theme.component.html',
 })
-export class ModifyThemeComponent {}
+export class ModifyThemeComponent {
+  @Input() theme: Theme;
+}
--- frontend/src/app/admin/themes/themes.component.ts
+++ frontend/src/app/admin/themes/themes.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { Theme } from 'src/models/quiz.model';
 @Component({
   selector: 'app-themes',
   templateUrl: './themes.component.html',
 })
 export class ThemesComponent {
+  @Output() modifyTheme: EventEmitter<Theme> = new EventEmitter<Theme>();
+
   modals = [
     {
       num: 1,
       title: 'Ajouter un thème',
+    },
+    {
+      num: 2,
+      title: 'Modifier un thème',
     },
   ];
   constructor(private router: Router) {}
 
   navigateAddTheme() {
     let dialog = document.getElementsByTagName('dialog')[this.modals[0].num];
+    dialog.showModal();
+  }
+
+  handleModifyTheme(theme: Theme) {
+    console.log(theme);
+    this.modifyTheme.emit(theme);
+    let dialog = document.getElementsByTagName('dialog')[this.modals[1].num];
     dialog.showModal();
   }
 }
