edab12c349180256337ea049002ff9af269b5b4f
Marc Pinet <marc.pinet@etu.univ-cotedazur.fr>
2023-06-17 13:54:06 +0200

    fix: removed unused .sh file #333

--- tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Démarrer le conteneur frontend-test-server
-docker compose up -d frontend-test-server
-
-# Une fois que le serveur de test est prêt, démarrer les tests e2e
-echo "Démarrage des tests e2e..."
-docker compose up frontend-test-e2e
