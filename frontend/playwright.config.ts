c48943ad47209f26a323343b7020f0e08f835c7a
Marc Pinet <marc.pinet@etu.univ-cotedazur.fr>
2023-06-17 13:55:40 +0200

    fix: headless mode for dockerization #333

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -4,7 +4,7 @@ const config: PlaywrightTestConfig = {
   reporter: [['html', { open: 'always' }]],
   timeout: 60000,
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     video: 'on-first-retry',
